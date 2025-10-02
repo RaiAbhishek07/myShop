202423260d566824da3d80a268e968a62b1f7983
abhishek rai <abhishekrai3ram@gmail.com>
2025-10-02 18:55:08 +0000

    The Netlify deploy errored, with the following guidance provided:
    You a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
+export const Logo = (props: React.HTMLAttributes<HTMLDivElement>) => (
     <div className="flex items-center" {...props}>
-        <span className="font-headline font-bold text-2xl tracking-tight">
+        <span className="font-headline font-bold text-2xl tracking-tight text-primary">
             Select Men's Tailor
         </span>
     </div>
