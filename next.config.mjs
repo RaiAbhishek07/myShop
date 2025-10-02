ce3c48aac0852f27cb44a6e1db74aa5f70cb0d11
abhishek rai <abhishekrai3ram@gmail.com>
2025-10-02 03:17:35 +0000

    provide methe path were i should dump the image
    use teh path url to di

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
